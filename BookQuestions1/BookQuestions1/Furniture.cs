a0cb22793ab8f10045f9fc2e7724c73358c2263f
hubbercraft <huberkyle06@gmail.com>
2013-10-21 08:24:56 -0400

    Did stuff

---
+++ BookQuestions1/BookQuestions1/Furniture.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BookQuestions
+{
+    class Furniture
+    {
+        static void Main(string[] args)
+        {
+            int Price = 0;
+            Console.WriteLine("(P)ine, (O)ak, (M)ahogany");
+            string input = Console.ReadLine();
+            if (input == "P")
+            {
+                Price += 100;
+                Console.Write(Convert.ToInt32(Price));
+            }
+            else if (input == "O")
+            {
+                Price += 225;
+                Console.Write(Convert.ToInt32(Price));
+            }
+            else if (input == "M")
+            {
+                Price += 310;
+                Console.Write(Convert.ToInt32(Price));
+            }
+            else
+            {
+                Price += 0;
+            }
+
+        }
+    }
+}
