8207385f8538408f14c173b8e1dac428282b4b03
hubbercraft <huberkyle06@gmail.com>
2013-10-21 08:42:45 -0400

    DSF

---
+++ BookQuestions1/CheckZips.cs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BookQuestions
+{
+    class CheckZips
+    {
+        static void Main(string[] args)
+        {
+            int[] ZipCodes = new int[10];
+            ZipCodes[0] = 12345;
+            ZipCodes[1] = 12346;
+            ZipCodes[2] = 12347;
+            ZipCodes[3] = 12348;
+            ZipCodes[4] = 12349;
+            ZipCodes[5] = 12350;
+            ZipCodes[6] = 12351;
+            ZipCodes[7] = 12352;
+            ZipCodes[8] = 12353;
+            ZipCodes[9] = 12354;
+
+            Console.WriteLine("WHAT is your zipcode?");
+            string input = Console.ReadLine();
+            for (int i = 0; i < ZipCodes; i++) ;
+
+        }
+    }
+}
