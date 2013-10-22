bf08cb4d882b27cb9d8de41af8a3b4f8d806cf38
hubbercraft <huberkyle06@gmail.com>
2013-10-22 08:38:40 -0400

    Started third project

---
+++ BookQuestions1/ChatAWhile.cs/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChatAWhile.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] AreaCodes = new int[6] {262, 414, 608, 715, 815, 920};
+            double[] Rates = new double[6] { .07, .10, .05, .16, .24, .14 };
+
+            Console.WriteLine("What is your area code and length of call?");
+
+        }
+    }
+}
