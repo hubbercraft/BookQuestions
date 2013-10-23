fb4566bf644eb66fceb01bf20efac572b12288d8
hubbercraft <huberkyle06@gmail.com>
2013-10-23 08:25:19 -0400

    FINISHED THE FOURTH. :D

---
+++ BookQuestions1/Commission.cs/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Commission.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Carprice;
+            double Commission;
+            double value;
+
+            Console.WriteLine("What is the price of the car?");
+            Carprice = Console.ReadLine();
+            value = Convert.ToDouble(Carprice);
+            
+            if (value <= 15000)
+            {
+                Commission = value * .05;
+            }
+            else if (value <= 24000 && value > 15000)
+            {
+                Commission = value * .07;
+            }
+
+            else 
+            {
+                Commission = value * .10;
+            }
+            Console.WriteLine("Your commission is: $" + Commission);
+        }
+    }
+}
