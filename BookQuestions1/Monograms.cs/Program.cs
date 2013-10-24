60162cddcfbf1600c8477e174b29947a9075880c
hubbercraft <huberkyle06@gmail.com>
2013-10-24 08:03:24 -0400

    Monogram finished

---
+++ BookQuestions1/Monograms.cs/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Monograms.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayMonogram("K", "W", "H");
+            DisplayMonogram("S", "T", "R");
+            Console.ReadLine();
+        }
+        public static void DisplayMonogram(string first, string middle, string last)
+        {
+            Console.WriteLine("** {0}. {1}. {2} **", first, middle, last);
+        }
+    }
+}
