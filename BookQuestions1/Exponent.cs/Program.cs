2a9d805e6cb1160b75113690e292633b036f7c16
hubbercraft <huberkyle06@gmail.com>
2013-10-24 08:22:34 -0400

    finished Exponentttttttttttttttttttttttttttttttttttttttttttttt

---
+++ BookQuestions1/Exponent.cs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Exponent.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            Console.WriteLine("Enter a number to be squared and cubed");
+            input = Console.ReadLine();
+
+            Console.WriteLine(Math.Pow(Convert.ToInt32(input), 2));
+            Console.WriteLine(Math.Pow(Convert.ToInt32(input), 3));
+        }
+    }
+}
