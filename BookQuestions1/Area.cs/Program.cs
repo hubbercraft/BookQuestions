a964c0ea076049ddead7a94cd121ef26e632f84c
hubbercraft <huberkyle06@gmail.com>
2013-10-30 21:56:14 -0400

    Finished area

--- BookQuestions1/Area.cs/Program.cs
+++ BookQuestions1/Area.cs/Program.cs
@@ -9,7 +9,26 @@ namespace Area.cs
     {
         static void Main(string[] args)
         {
-
+            public static void Main()
+        {
+            int width = 7;
+            int height = 2;
+            ComputeArea(width, height);
+            ComputeArea(Convert.ToDouble(width), Convert.ToDouble(height));
+            ComputeArea(Convert.ToDouble(width), height);
+        }
+        public static void ComputeArea(int w, int h)
+        {
+            int area = w * h;
+        }
+        public static void ComputeArea(double w, double h)
+        {
+            double area = w * h;
+        }
+        public static void ComputeArea(double w, int h)
+        {
+            double area = w * h;
+        }
         }
     }
 }
